82241a5c8e09366c2020cdd496dc57e5c3fc8bbe
Paul Gaida <paul.gaida@dsmz.de>
2023-09-15 11:22:19 +0200

    creating custom components and nesting them.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'learn-angular'`, () => {
+  it(`should have as name 'Miguel'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('learn-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('learn-angular app is running!');
+    expect(app.name).toEqual('Miguel');
   });
 });
