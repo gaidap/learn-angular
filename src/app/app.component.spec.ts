e38417859852848ab69fab5a1bcf12022e8d4bce
Paul Gaida <pc.gaida@gmail.com>
2023-09-17 15:00:05 +0200

    Learning databinding.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,10 +11,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as name 'Miguel'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.name).toEqual('Miguel');
-  });
 });
