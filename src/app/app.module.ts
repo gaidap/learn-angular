5003220db375aa46942e3670daf0f0ed0a8ce222
Paul Gaida <paul.gaida@dsmz.de>
2023-09-18 11:39:27 +0200

    Reset App.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'; // <-- Import FormsModule
 
 import {AppComponent} from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
