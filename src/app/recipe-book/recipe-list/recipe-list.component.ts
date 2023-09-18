1dea0fdf3bc0d3dd39cf66931b33882ecb2ddeb7
Paul Gaida <paul.gaida@dsmz.de>
2023-09-18 13:42:50 +0200

    Corrected `Recipe` model and updated related test
    Updated `Recipe` model to require name, description, and image path parameters on instance creation. As a result, adjusted the tests for the `Recipe` model to pass these required parameters. Moreover, Imported `Recipe` in the 'recipe-list.component' and initialized empty array of 'recipes' to store instances of 'Recipe' to ensure correct implementation and easier manipulation of recipes.

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Recipe} from "../model/recipe";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [];
 
 }
